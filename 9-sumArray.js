648772cda434e3a1e9e6fcfbeaacf9a76f9e322b
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-01-30 20:16:03 +0700

    day 9/366

---
+++ 9-sumArray.js
@@ -0,0 +1,22 @@
+// WPU CHALLENGE 2024
+// 9/366
+// https://www.codewars.com/kata/576b93db1129fcf2200001e6
+
+function sumArray(array) {
+  if (!array) {
+    return 0;
+  }
+
+  array.sort(function (a, b) {
+    return a - b;
+  });
+
+  let total = 0;
+  for (let i = 1; i < array.length - 1; i++) {
+    total += array[i];
+  }
+
+  return total;
+}
+
+console.log(sumArray([6, 2, 1, 8, 10]));
