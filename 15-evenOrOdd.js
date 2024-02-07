5558e85120e7221fe9f75ef0c27e2907fdda1e69
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-02-07 09:58:38 +0700

    day 15/366

---
+++ 15-evenOrOdd.js
@@ -0,0 +1,15 @@
+// WPU CHALLENGE 2024
+// 15/366
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+// function evenOrOdd(number) {
+//   if (number % 2 == 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// }
+
+const evenOrOdd = (number) => (number % 2 == 0 ? "Even" : "Odd");
+
+console.log(evenOrOdd(4));
