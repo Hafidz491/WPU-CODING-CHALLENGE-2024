19ba33c69074ec2cca2d4f96dd31c253820b22d1
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-02-13 09:15:22 +0700

    day 18/366

---
+++ 18-stringToArray.js
@@ -0,0 +1,16 @@
+// WPU CHALLENGE 2024
+// 18/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// function stringToArray(string) {
+//   // code code code
+//   const result = [];
+//   for (let i = 0; i <= string.length; i++) {
+//     result.push(string[i]);
+//   }
+//   return string.split(" ");
+// }
+
+const stringToArray = (string) => string.split(" ");
+
+console.log(stringToArray("Robin Singh"));
