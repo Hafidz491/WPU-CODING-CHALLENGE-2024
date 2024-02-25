090bd5e3451511474316ba48c4fbe530a3f80c92
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-02-25 07:34:22 +0700

    day 9/366

--- 9-sumArray.js
+++ 9-sumArray.js
@@ -2,19 +2,27 @@
 // 9/366
 // https://www.codewars.com/kata/576b93db1129fcf2200001e6
 
-function sumArray(array) {
-  if (!array) {
-    return 0;
-  }
+// function sumArray(array) {
+//   if (!array) {
+//     return 0;
+//   }
 
-  array.sort((a, b) => a - b);
+//   array.sort((a, b) => a - b);
 
-  let total = 0;
-  for (let i = 1; i < array.length - 1; i++) {
-    total += array[i];
-  }
+//   let total = 0;
+//   for (let i = 1; i < array.length - 1; i++) {
+//     total += array[i];
+//   }
 
-  return total;
-}
+//   return total;
+// }
+
+const sumArray = (array) =>
+  array == null || array.length <= 2
+    ? 0
+    : array
+        .sort((a, b) => a - b)
+        .slice(1, -1)
+        .reduce((a, b) => a + b);
 
 console.log(sumArray([6, 2, 1, 8, 10]));
