19b40f24b9c4ddd7faa1844daa5ba9968e86cfc5
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-02-25 17:10:32 +0700

    day 30/366

---
+++ 30-digitize.js
@@ -0,0 +1,7 @@
+// WPU CHALLENGE 2024
+// 30/366
+// https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+
+const digitize = (n) => n.toString().split("").reverse().map(Number);
+
+console.log(digitize(35231));
