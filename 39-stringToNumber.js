65b82b0bda3bc999c651054bdf1a3219efcd111d
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-03-09 05:53:04 +0700

    day 39/366

---
+++ 39-stringToNumber.js
@@ -0,0 +1,12 @@
+// WPU CHALLENGE 2024
+// 39/366
+// https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+// const stringToNumber = function (str) {
+//   // put your code here
+//   return +str;
+// };
+
+const stringToNumber = (str) => +str;
+
+console.log(stringToNumber("1234"));
