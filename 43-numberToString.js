f54081a5fcff4b89332288d80623a2720701aec6
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-03-11 06:18:49 +0700

    day 43/366

---
+++ 43-numberToString.js
@@ -0,0 +1,12 @@
+// WPU CHALLENGE 2024
+// 43/366
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+// function numberToString(num) {
+//   // Return a string of the number here!
+//   return num.toString();
+// }
+
+const numberToString = (num) => "" + num;
+
+console.log(numberToString(123));
