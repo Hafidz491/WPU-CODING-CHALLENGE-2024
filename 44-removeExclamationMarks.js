e822e3bdc28595efc5c1ecd4f8111753bca34e55
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-03-26 11:01:19 +0700

    day 44/366

---
+++ 44-removeExclamationMarks.js
@@ -0,0 +1,17 @@
+// WPU CHALLENGE 2024
+// 44/366
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+// function removeExclamationMarks(s) {
+//   let result = "";
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] !== "!") {
+//       result += s[i];
+//     }
+//   }
+//   return result;
+// }
+
+const removeExclamationMarks = (s) => s.split("!").join("");
+
+console.log(removeExclamationMarks("Hello World!"));
