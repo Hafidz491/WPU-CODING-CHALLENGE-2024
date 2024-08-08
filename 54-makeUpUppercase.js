b5284f06e04711eee9ae509376cce9eedd5ad2c4
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-08-09 05:34:14 +0700

    day 54/366

---
+++ 54-makeUpUppercase.js
@@ -0,0 +1,9 @@
+// WPU CHALLENGE 2024
+// 54/366
+//https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+console.log(makeUpperCase("hello"));
