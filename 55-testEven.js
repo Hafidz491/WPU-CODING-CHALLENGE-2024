b7a1eca6dd911f74e41b2d1291bf6e081e893cda
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-08-09 05:38:18 +0700

    day 55/366

---
+++ 55-testEven.js
@@ -0,0 +1,18 @@
+// WPU CHALLENGE 2024
+// 55/366
+//https://www.codewars.com/kata/555a67db74814aa4ee0001b5
+
+// function testEven(n) {
+//   //Your awesome code here!
+//   if (n % 2 == 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// const testEven = (n) => (n % 2 === 0 ? true : false);
+
+const testEven = (n) => n % 2 === 0;
+
+console.log(testEven(1));
