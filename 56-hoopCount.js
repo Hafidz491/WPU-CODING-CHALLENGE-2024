94ed3d77c8074635bc98938e6d7b8a79c93bf741
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-08-09 05:45:12 +0700

    day 56/366

---
+++ 56-hoopCount.js
@@ -0,0 +1,17 @@
+// WPU CHALLENGE 2024
+// 56/366
+//https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+
+// function hoopCount(n) {
+//   //your code goes here
+//   if (n >= 10) {
+//     return "Keep at it until you get it";
+//   } else {
+//     return "Keep at it until you get it";
+//   }
+// }
+
+const hoopCount = (n) =>
+  n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+
+console.log(hoopCount(10));
