c85d1403f111dcfca7eee243680735c3155a8cec
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-08-15 06:20:32 +0700

    day 63/366

---
+++ 63-paperwork.js
@@ -0,0 +1,7 @@
+// WPU CHALLENGE 2024
+// 63/366
+//https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+
+const paperwork = (n, m) => (n < 0 || m < 0 ? 0 : n * m);
+
+console.log(paperwork(5, 5));
