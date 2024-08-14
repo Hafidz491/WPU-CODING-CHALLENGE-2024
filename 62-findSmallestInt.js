8af7dfde6e928a42f22b9b55c07e67e50f6e4403
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-08-15 06:12:39 +0700

    day 62/366

---
+++ 62-findSmallestInt.js
@@ -0,0 +1,11 @@
+// WPU CHALLENGE 2024
+// 62/366
+//https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+
+// function findSmallestInt(arr) {
+//   return Math.min(...arr);
+// }
+
+const findSmallestInt = (arr) => Math.min(...arr);
+
+console.log(findSmallestInt([78, 56, 232, 12, 8]));
