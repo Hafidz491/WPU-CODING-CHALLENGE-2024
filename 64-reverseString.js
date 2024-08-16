3c462812be0da7dfb042d88a246f105520b96569
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-08-16 19:20:20 +0700

    day 64/366

---
+++ 64-reverseString.js
@@ -0,0 +1,11 @@
+// WPU CHALLENGE 2024
+// 64/366
+//https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+
+// function solution(str) {
+//   return str.split("").reverse().join("");
+// }
+
+const solution = (str) => str.split("").reverse().join("");
+
+console.log(solution("world"));
