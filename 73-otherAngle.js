c5b97be660beb024c30d0b5a3eb79a82bf2d7e43
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-12-26 23:51:48 +0700

    day 73/366

---
+++ 73-otherAngle.js
@@ -0,0 +1,9 @@
+// WPU CHALLENGE 2024
+// 73/366
+//https://www.codewars.com/kata/5a023c426975981341000014
+
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
+
+console.log(otherAngle(43, 78));
